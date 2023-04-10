3fc3f8d4133fc62c63f493bd67b0c356d466533f
Daniel Olvera <danielolvera311@gmail.com>
2023-04-10 13:54:43 -0500

    Added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import {Routes,Route} from "react-router-dom"
 import { Home } from './pages/Home'
 import { Store } from './pages/Store'
 import { About } from './pages/About'
+import { Navbar } from './components/Navbar'
 
 //handles routes and components
 function App() {
-  return(<Container className='mb-4'>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/store' element={<Store/>}/>
-      <Route path='/about' element={<About/>}/>
-    </Routes>
-  </Container>)
-  
+  return(
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/store' element={<Store/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </Container>
+    </>
+  )  
 }
 
 export default App
